2937029c3f13bd58b4c4cc805de7098831c50751
Luis <ldsilva@luiss-mbp.home>
2014-10-08 14:42:02 +0100

    First lab done and working with nested conditionals.

--- Code.j
+++ Code.j
@@ -22,3 +22,15 @@
        ; place your bytecodes here
 
        ; START
+sipush 0
+       ; END
+
+
+       ; convert to String;
+       invokestatic java/lang/String/valueOf(I)Ljava/lang/String;
+       ; call println 
+       invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V
+
+       return
+
+.end method
--- src/Main.java
+++ src/Main.java
@@ -14,46 +14,6 @@ public class Main {
 		Parser parser = new Parser(System.in);
 		System.out.println("Welcome.");
 		
-//		TEXT EXPRESSIONS. COMMENT OUT ONE OF THEM TO USE
-		
-//		ASTNode exp = new ASTCond(
-//		new ASTEq(new ASTPlus(new ASTNum(IValue.fromInteger(3)), new ASTNum(IValue.fromInteger(3))), new ASTDiv(new ASTNum(IValue.fromInteger(12)), new ASTPlus(new ASTNum(IValue.fromInteger(-1)), new ASTNum(IValue.fromInteger(3))))),
-//		new ASTPlus(new ASTNum(IValue.fromInteger(1)), new ASTNum(IValue.fromInteger(2))),
-//		new ASTMul(new ASTNum(IValue.fromInteger(2)), new ASTNum(IValue.fromInteger(7)))
-//		);
-//		ASTNode exp = new ASTCond(
-//				new ASTEq(new ASTNum(IValue.fromInteger(99)), new ASTNum(IValue.fromInteger(100))),
-//				new ASTCond
-//				(
-//						new ASTGr(new ASTNum(IValue.fromInteger(20)), new ASTNum(IValue.fromInteger(15))),
-//						new ASTNum(IValue.fromInteger(22)),
-//						new ASTNum(IValue.fromInteger(92))
-//				),
-//				new ASTNum(IValue.fromInteger(-1))
-//		);
-		
-//		ASTNode exp = new ASTCond(
-//				new ASTEq(new ASTNum(IValue.fromInteger(100)), new ASTNum(IValue.fromInteger(100))),
-//				new ASTCond
-//				(
-//						new ASTGr(
-//								new ASTCond(
-//										new ASTAnd(
-//												new ASTLseq(new ASTNum(IValue.fromInteger(45)), new ASTNum(IValue.fromInteger(45))),
-//												new ASTEq(new ASTNum(IValue.fromInteger(99)), new ASTNum(IValue.fromInteger(100)))
-//										),
-//										new ASTNum(IValue.fromInteger(20)),
-//										new ASTNum(IValue.fromInteger(850))),
-//								new ASTNum(IValue.fromInteger(15))),
-//						new ASTNum(IValue.fromInteger(22)),
-//						new ASTNum(IValue.fromInteger(92))
-//				),
-//				new ASTNum(IValue.fromInteger(-1))
-//		);
-		
-//		System.out.println("Ok:  "+exp.accept(new UnparseVisitor()));
-//		System.out.println("Val: "+exp.accept(new EvalVisitor()));
-//		System.out.println("Code:\n"+exp.accept(new CompilerVisitor()));
 		CodeBlock cb;
 		while (true) {
 			try {
@@ -61,7 +21,6 @@ public class Main {
 				System.out.println("Ok:  "+exp.accept(new UnparseVisitor()));
 				System.out.println("Val: "+exp.accept(new EvalVisitor()));
 				cb = exp.accept(new CompilerVisitor());
-//				System.out.println("Code:\n"+cb);
 				cb.writeToFile(new File("Code.j"));
 				System.out.println("Code written to file \"Code.j\" in the project or bin directory.");
 			} catch (Error e) {
